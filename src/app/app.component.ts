6dae49111020a8d067608bde316fcf3f4d27a00c
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-26 19:49:51 +0530

    feat: implement main layout with sidebar, header, dashboard cards, and user management components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { DashboardCardsComponent } from "./components/dashboard-cards/dashboard-cards.component";
+import { UserManagementComponent } from "./components/user-management/user-management.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [SidebarComponent, HeaderComponent, DashboardCardsComponent, UserManagementComponent]
 })
 export class AppComponent {
   title = 'frontend';
