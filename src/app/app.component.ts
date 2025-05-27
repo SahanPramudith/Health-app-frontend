797a3bfd28a7ec63f2052ec6d0377774c430b354
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-27 11:06:15 +0530

    feat: update routing and sidebar navigation for patient management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { DashboardCardsComponent } from "./components/dashboard-cards/dashboard-cards.component";
 import { UserManagementComponent } from "./components/user-management/user-management.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [SidebarComponent, HeaderComponent, DashboardCardsComponent, UserManagementComponent]
+  imports: [SidebarComponent, HeaderComponent, DashboardCardsComponent, UserManagementComponent,RouterOutlet]
 })
 export class AppComponent {
   title = 'frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PatientManagementComponent } from './components/patient-management/patient-management.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'patients',
+        component:PatientManagementComponent
+    },
+    {
+        path: 'user-management',
+        component:UserManagementComponent
+    },
+    
+];
