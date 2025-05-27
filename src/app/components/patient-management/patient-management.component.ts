b94148f9b1e451db0daffd6b6bed307ffe230748
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-27 16:21:51 +0530

    feat: implement patient registration form with validation and modal

--- src/app/components/patient-management/patient-management.component.ts
+++ src/app/components/patient-management/patient-management.component.ts
@@ -1,11 +1,30 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-patient-management',
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf ],
   templateUrl: './patient-management.component.html',
   styleUrl: './patient-management.component.css'
 })
 export class PatientManagementComponent {
 
+  patientForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.patientForm = this.fb.group({
+      fullName: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(0)]],
+      gender: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.patientForm);
+  }
+    
 }
