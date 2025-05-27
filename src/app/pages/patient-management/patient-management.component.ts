7e95c15174fa45b2a80b930c423c1a589baaefb0
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-27 09:09:02 +0530

    feat: add patient management component with HTML, CSS, and unit tests

---
+++ src/app/pages/patient-management/patient-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-management',
+  imports: [],
+  templateUrl: './patient-management.component.html',
+  styleUrl: './patient-management.component.css'
+})
+export class PatientManagementComponent {
+
+}
