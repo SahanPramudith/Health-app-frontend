7a40697ffa3e15e1a8b44bf2ae40dcbdaf4b1297
sahanpramudith <pramuditsahan466@gmail.com>
2025-06-06 20:13:59 +0530

    feat: add appointment form component and update routing for appointments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PatientManagementComponent } from './components/patient-management/patient-management.component';
 import { UserManagementComponent } from './components/user-management/user-management.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
 
 export const routes: Routes = [
     {
@@ -8,8 +9,8 @@ export const routes: Routes = [
         component:PatientManagementComponent
     },
     {
-        path: 'user-management',
-        component:UserManagementComponent
+        path: 'appointment',
+        component:AppointmentFormComponent
     },
     
 ];
