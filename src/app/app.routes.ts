d6974b2369771cab9da24763075fe0cb849f02eb
sahanpramudith <pramuditsahan466@gmail.com>
2025-06-07 21:51:34 +0530

    feat: add dashboard cards component with tab navigation for patients and appointments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PatientManagementComponent } from './components/patient-management/patient-management.component';
 import { UserManagementComponent } from './components/user-management/user-management.component';
 import { AppointmentFormComponent } from './components/appointment-form/appointment-form.component';
+import { DashboardCardsComponent } from './components/dashboard-cards/dashboard-cards.component';
 //import { AppointmentFormComponent } from './components/appointment-form/appointment-form.component';
 
 export const routes: Routes = [
@@ -12,6 +13,10 @@ export const routes: Routes = [
     {
         path: 'appointment',
         component:AppointmentFormComponent
+    },
+    {
+        path: 'dashboardCard',
+        component:DashboardCardsComponent
     },
     
 ];
--- src/app/components/dashboard-cards/dashboard-cards.component.ts
+++ src/app/components/dashboard-cards/dashboard-cards.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RegistePateintService } from '../../services/Patient/registe-pateint.service';
+import { AppointmentService } from '../../services/appointment/appointment.service';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-cards',
-  imports: [],
+  imports: [NgClass,NgIf,NgFor],
   templateUrl: './dashboard-cards.component.html',
   styleUrl: './dashboard-cards.component.css'
 })
-export class DashboardCardsComponent {
+export class DashboardCardsComponent implements OnInit {
+  patients: any[] = [];
+  appointments: any[] = [];
+
+  constructor(private patientservice: RegistePateintService, private appointmentService: AppointmentService) { }
+
+  ngOnInit(): void {
+    this.patientservice.getAllPatients().subscribe(date => {
+      this.patients = date;
+      //this.appointmentService.().subscribe(data => this.appointments = data);
+
+    })
+  }
+
+  activeTab = 'dashboard';
+
+  selectTab(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.activeTab = value;
+  }
+
+
 
 }
